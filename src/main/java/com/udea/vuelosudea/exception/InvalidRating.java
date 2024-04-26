469fa9352d7432c9e40f324fe8acd28f3ed37cb6
DB <diego.botia@udea.edu.co>
2024-04-26 13:36:16 -0500

    Primer commit

---
+++ src/main/java/com/udea/vuelosudea/exception/InvalidRating.java
@@ -0,0 +1,15 @@
+package com.udea.vuelosudea.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+public class InvalidRating extends RuntimeException{
+    public InvalidRating(){
+        super();
+    }
+
+    public InvalidRating(String mensaje){
+        super(mensaje);
+    }
+}
